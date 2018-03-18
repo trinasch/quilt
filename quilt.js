7ed8900e625bc5d51bef15077a1900808f8cb687
kschwieterman <trinasch@hotmail.com>
2018-03-17 20:13:26 -0700

    responsive image test

---
+++ quilt.js
@@ -0,0 +1,18 @@
+//define quilt variable with img source array
+
+var image = [
+  'images/quilt1.jpeg',
+  'images/quilt2.jpeg',
+  'images/quilt3.jpeg',
+  'images/quilt4.jpeg',
+  'images/quilt5.jpeg',
+  'images/quilt6.jpeg',
+  'images/quilt7.jpeg',
+  'images/quilt8.jpeg',
+  'images/quilt9.jpeg'
+]
+
+function mix(images){
+  
+  
+}
